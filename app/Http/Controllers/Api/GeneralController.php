73dd19d10bf3a2091053e5fcd6abbe412d6347da
Niyaz <hungry_la@mail.ru>
2024-09-23 15:14:04 +0500

    Deploy project

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -9,14 +9,16 @@ use Illuminate\Support\Facades\Log;
 class GeneralController extends Controller
 {
     public function test(){
-        $out1 = exec('cd /var/www/www-root/data/www/clinic-dev.proviant-pay.ru/');
+//        $out1 = exec('cd /var/www/www-root/data/www/clinic-dev.proviant-pay.ru/');
+        $out5 = exec('php -v');
         $out4 = exec('pwd');
         $out2 = exec('composer update');
         $out3 = exec('php artisan migrate');
         Log::info("Build Success");
-        Log::info("Out1". $out1);
+//        Log::info("Out1". $out1);
+        Log::info("Out5". $out5);
+        Log::info("Out4". $out4);
         Log::info("Out2". $out2);
         Log::info("Out3". $out3);
-        Log::info("Out4". $out4);
     }
 }
