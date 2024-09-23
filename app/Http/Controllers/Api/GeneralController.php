70e1be41afe7f41b76a5e7bc4c0dd62064e0fb46
Niyaz <hungry_la@mail.ru>
2024-09-23 16:05:23 +0500

    Deploy project

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -6,14 +6,19 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Process;
 
 class GeneralController extends Controller
 {
     public function test(){
         exec('cd /var/www/www-root/data/www/clinic-dev.proviant-pay.ru/');
         $out1 = exec('git pull origin main');
+//        $proc = new Process(['git', 'pull']);
 //        $out2 = exec('composer update');
 //        $out3 = Artisan::call('migrate');
+        Process::run('bash deploy.sh');
+        $out = Process::run('cd /var/www/www-root/data/www/clinic-dev.proviant-pay.ru/');
+        Log::info($out);
         Log::info("Build Success");
 //        Log::info("Out2 ". $out2);
         Log::info("Out1 ". $out1);
