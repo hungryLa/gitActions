815c398554b5743408f66da77f06989a48fc2e97
Niyaz <hungry_la@mail.ru>
2024-09-23 15:42:28 +0500

    Deploy project

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -10,9 +10,12 @@ use Illuminate\Support\Facades\Artisan;
 class GeneralController extends Controller
 {
     public function test(){
+        $out1 = exec('cd /var/www/www-root/data/www/clinic-dev.proviant-pay.ru/');
+        $out4 = exec('pwd');
         $out2 = exec('composer update');
         $out3 = Artisan::call('migrate');
         Log::info("Build Success");
+        Log::info("Out4 ". $out4);
         Log::info("Out2 ". $out2);
         Log::info("Out3 ". $out3);
     }
